42cb86a3b24001a61209a04fa0b0e0aec6f35526
willianproni <wifepro@hotmail.com>
2024-02-16 23:02:01 -0300

    Criando theme global e utilizando ThemeProvider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,14 @@
 import { PropsWithChildren } from 'react'
 
 import GlobalStyles from '@/styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from '@/styles/theme'
 
 export function Provider({ children }: PropsWithChildren) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       {children}
-    </>
+    </ThemeProvider>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,22 +1,57 @@
 'use client'
 
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+/* poppins-300 - latin */
+@font-face {
+  font-display: swap;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: url('/fonts/poppins-v20-latin-300.woff2') format('woff2');
+}
+/* poppins-regular - latin */
+@font-face {
+  font-display: swap;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('/fonts/poppins-v20-latin-regular.woff2') format('woff2');
+}
+/* poppins-600 - latin */
+@font-face {
+  font-display: swap;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: url('/fonts/poppins-v20-latin-600.woff2') format('woff2');
+}
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
-  html {
-    font-size: 62.5%; // 1rem = 10px == 10/16px = 62.5% (1.4rem = 14px)
-  }
-  html, body, #__next {
-    height: 100%;
-  }
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%; // 1rem = 10px == 10/16px = 62.5% (1.4rem = 14px)
+    }
+
+    body {
+      font-family: ${theme.font.family};
+      font-size: : ${theme.font.sizes.medium};
+    }
+  `}
+
+
 `
 
 export default GlobalStyles
