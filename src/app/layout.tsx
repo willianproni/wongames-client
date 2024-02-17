3eac2d4d3212f2145d96809fab593857839ac601
willianproni <wifepro@hotmail.com>
2024-02-16 23:01:36 -0300

    Configurando site em PT-Br

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
 import StyledComponentsRegistry from '@/lib/registry'
 import { Provider } from './providers'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="pt-br">
+      <body>
         <StyledComponentsRegistry>
           <Provider>{children}</Provider>
         </StyledComponentsRegistry>
